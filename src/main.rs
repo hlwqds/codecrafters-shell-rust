b781bd772189e1d1c3d05cbcb2b3022253bcf639
hlwqds <hl-ljj@outlook.com>
2026-05-06 11:12:24 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process::{self, ExitCode};
+use std::process::{self};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    let mut command = String::new();
+    let mut buffer = String::new();
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        command.clear();
+        buffer.clear();
         // Wait for user input
-        io::stdin().read_line(&mut command).unwrap();
-        let cmd = command.trim();
+        io::stdin().read_line(&mut buffer).unwrap();
+        let args: Vec<&str> = buffer.split_whitespace().collect();
+
+        if args.is_empty() {
+            continue;
+        }
+
+        let cmd = args[0];
         if cmd == "exit" {
             process::exit(0)
+        } else if cmd == "echo" {
+            println!("{}", args[1..].join(" "));
         } else {
             println!("{}: command not found", cmd);
         }
